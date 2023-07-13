9ac8ccf051ede1a0a54a41d1ee6e61e2f0792ed8
jmore3 <juanmore3@gmail.com>
2023-07-13 17:11:54 -0600

    FileReader class comments template

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,21 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The FileReader class is responsible for reading bank information from a CSV file.
+ */
 
 public class FileReader {
-    
-        static ArrayList<Account> users = new ArrayList<Account>(); 
-        FileReader file;
-        Account user;
-        //read through file
-        // add to user info
-        // i = 0;
-        // foreach (fileRow) {
-        //     user.personInfo(fileRow[i]);
-        //     user.personInfo(fileRow[i]);
-        //     user.checkings(fileRow[checkingAccountNumber],fileRow[checkingStartingBalance]);
-        //     i++;
-        // }
 
-        public static ArrayList<Account> getUsers(){
-            return users;
+    private static final String CSV_FILE_PATH = "CS 3331 - Bank Users.csv";
+
+    /**
+     * Reads the bank user information from the CSV file and returns a list of Account objects.
+     *
+     * @return The list of Account objects containing the bank user information.
+     */
+    public static List<Account> getUsers(){
+        List<Account> users = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(CSV_FILE_PATH))){
+            String line;
+            boolean headerLine = true;
+
+            while ((line = reader.readLine()) != null) {
+                if (headerLine) {
+                    headerLine = false;
+                    continue; //Skips the header line
+                }
+                //Split the CSV line into an array of values
+                String[] data = line.split(",");
+
+                // Extract the bank user information from the data array
+                
+                // Create the account and person objects
+
+                // Set the account and person details
+
+                users.add(account);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the file reading exception as needed
         }
+        return users;
+    } 
 }
