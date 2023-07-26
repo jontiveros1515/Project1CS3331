563fdf48d74dc29a1556b37cdcfa95d98ab6c134
jmore3 <juanmore3@gmail.com>
2023-07-26 14:33:10 -0600

    CSVReader with User Transaction stuff

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -1,62 +1,86 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
- * The FileReader class is responsible for reading bank information from a CSV file.
+ * The CSVReader class is responsible for reading and writing bank user information from and to a CSV file.
+ * It provides methods for reading user information from the CSV file and updating the CSV file with new information.
+ * It also includes a method to log information to a text file.
  * 
- * @author Juan Moreno, Javier Ontiveros
- * @version 1.0.3
+ * This class uses the Account, Customer, Checking, Saving, and Credit classes to store and represent the bank user information.
+ * The bank user information is read from the CSV file and stored in a list of Account objects.
+ * The updated user information is written back to the CSV file.
+ * 
+ * The CSV file should have the following columns:
+ * "Identification Number,First Name,Last Name,Date of Birth,Address,Phone Number,
+ * Checking Account Number,Checking Starting Balance,
+ * Savings Account Number,Savings Starting Balance,
+ * Credit Account Number,Credit Max,Credit Starting Balance"
+ * 
+ * The text log file will store logged information.
+ * 
+ * @author Juan Moreno, Javier Ontiveros, Alejandro Gonzalez
+ * @version 2.1.0
  */
-
 public class CSVReader {
-
-    static File file = new File("src/CS 3331 - Bank Users.csv");
+    private static File file = new File("BankUsers.csv");
+    private static File logFile = new File("TextLog.txt");
 
     /**
      * Reads the bank user information from the CSV file and returns a list of Account objects.
      *
      * @return The list of Account objects containing the bank user information.
      */
-    public static ArrayList<Account> getUsers(){
+    public static ArrayList<Account> getUsers() {
         ArrayList<Account> users = new ArrayList<>();
-
 
         try {
             FileReader fr = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fr);  
+            BufferedReader reader = new BufferedReader(fr);
             String line;
             boolean headerLine = true;
-            String address = ""; 
 
             while ((line = reader.readLine()) != null) {
-
                 if (headerLine) {
                     headerLine = false;
-                    continue; //Skips the header line
+                    continue; // Skip the header line
                 }
-
-                //Split the CSV line into an array of values
-                address = line.substring(line.indexOf("\""), line.indexOf("\"", line.indexOf("\"") + 2));
 
                 String[] data = line.split(",");
 
                 // Extract the bank user information from the data array
-                Person person = new Person(data[1], data[2], data[3], address, data[7]);
-                Customer customer = new Customer(person, Integer.parseInt(data[0]));
-                Checking checking = new Checking(Integer.parseInt(data[8]), Double.parseDouble(data[9]));
-                Saving savings = new Saving(Integer.parseInt(data[10]),Double.parseDouble(data[11]));
-                Credit credit = new Credit(Integer.parseInt(data[12]), Integer.parseInt(data[13]), Double.parseDouble(data[14]));
+                int identificationNumber = Integer.parseInt(data[0]);
+                String firstName = data[1];
+                String lastName = data[2];
+                String dateOfBirth = data[3];
+                String address = data[4];
+                String phoneNumber = data[5];
 
-                Account user = new Account(customer, checking, savings, credit);
+                int checkingAccountNumber = Integer.parseInt(data[6]);
+                double checkingStartingBalance = Double.parseDouble(data[7]);
+                Checking checking = new Checking(checkingAccountNumber, checkingStartingBalance);
+
+                int savingsAccountNumber = Integer.parseInt(data[8]);
+                double savingsStartingBalance = Double.parseDouble(data[9]);
+                Saving saving = new Saving(savingsAccountNumber, savingsStartingBalance);
+
+                int creditAccountNumber = Integer.parseInt(data[10]);
+                int creditMax = Integer.parseInt(data[11]);
+                double creditStartingBalance = Double.parseDouble(data[12]);
+                Credit credit = new Credit(creditAccountNumber, creditMax, creditStartingBalance);
+
+                Person person = new Person(firstName, lastName, dateOfBirth, address, phoneNumber)
+                Customer customer = new Customer(person, identificationNumber);
+                Account user = new Account(customer, checking, saving, credit);
 
                 users.add(user);
-            
             }
             reader.close();
         } catch (IOException e) {
@@ -67,18 +91,18 @@ public class CSVReader {
     }
 
     /**
-    * Updates the bank user information and writes it to the CSV file.
+     * Updates the bank user information and writes it to the CSV file.
      *
      * @param users The list of Account objects containing the updated bank user information.
      */
     public static void updateUsers(ArrayList<Account> users) {
-        File updatedFile = new File("src/Updated Bank Users.csv");
+        File updatedFile = new File("UpdatedBankUsers.csv");
 
         try {
             FileWriter fw = new FileWriter(updatedFile);
             BufferedWriter writer = new BufferedWriter(fw);
 
-             // Write the header line
+            // Write the header line
             writer.write("Identification Number,First Name,Last Name,Date of Birth,Address,Phone Number,Checking Account Number,Checking Starting Balance,Savings Account Number,Savings Starting Balance,Credit Account Number,Credit Max,Credit Starting Balance");
             writer.newLine();
 
@@ -90,18 +114,18 @@ public class CSVReader {
                 Credit credit = user.getCredit();
 
                 String line = customer.getIdentificationNumber() + ","
-                    + customer.getFirstName() + ","
-                    + customer.getLastName() + ","
-                    + customer.getDateOfBirth() + ","
-                    + customer.getAddress() + ","
-                    + customer.getPhoneNumber() + ","
-                    + checking.getCheckingAccountNumber() + ","
-                    + checking.getCheckingStartingBalance() + ","
-                    + saving.getSavingsAccountNumber() + ","
-                    + saving.getSavingsStartingBalance() + ","
-                    + credit.getCreditAccountNumber() + ","
-                    + credit.getCreditMax() + ","
-                    + credit.getCreditStartingBalance();
+                        + customer.getFirstName() + ","
+                        + customer.getLastName() + ","
+                        + customer.getDateOfBirth() + ","
+                        + customer.getAddress() + ","
+                        + customer.getPhoneNumber() + ","
+                        + checking.getCheckingAccountNumber() + ","
+                        + checking.getCheckingStartingBalance() + ","
+                        + saving.getSavingsAccountNumber() + ","
+                        + saving.getSavingsStartingBalance() + ","
+                        + credit.getCreditAccountNumber() + ","
+                        + credit.getCreditMax() + ","
+                        + credit.getCreditStartingBalance();
 
                 writer.write(line);
                 writer.newLine();
@@ -113,15 +137,15 @@ public class CSVReader {
             // Handle the file writing exception as needed
         }
     }
-    
+
     /**
      * Writes the log string to a text file named "TextLog.txt".
      *
-     * @param log The log string to be written into text file.
+     * @param log The log string to be written into the text file.
      */
     public static void textLog(String log) {
         try {
-            FileWriter fw = new FileWriter("TextLog.txt", true);
+            FileWriter fw = new FileWriter(logFile, true);
             BufferedWriter writer = new BufferedWriter(fw);
 
             writer.write(log);
@@ -132,5 +156,66 @@ public class CSVReader {
             e.printStackTrace();
             // Handle the file writing exception as needed
         }
+    }
+    /**
+     * Creates a user transactions file (.txt) with the specified information.
+     *
+     * @param account  The user's account information.
+     * @param startingBalance  The starting balance of the account (at the beginning of the session).
+     * @param endingBalance  The ending balance of the account (at the time requested).
+     * @param transactions  The list of transactions for the user.
+     */
+    public static void createUserTransactionsFile(Account account, double startingBalance, double endingBalance, String transactions) {
+        Customer customer = account.getCustomer();
+        String firstName = customer.getFirstName();
+        String lastName = customer.getLastName();
+        String fileName = firstName + "_" + lastName + "_Transactions.txt";
+
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            fw.write("User Transactions for: " + firstName + " " + lastName);
+            fw.write(System.lineSeparator());
+            fw.write("Account Information:");
+            fw.write(System.lineSeparator());
+            fw.write(customerInformationToString(customer));
+            fw.write(System.lineSeparator());
+            fw.write("Starting Balance: " + startingBalance);
+            fw.write(System.lineSeparator());
+            fw.write(transactions);
+            fw.write(System.lineSeparator());
+            fw.write("Ending Balance: " + endingBalance);
+            fw.write(System.lineSeparator());
+            fw.write("Date of Statement: " + getCurrentDateTime());
+            fw.write(System.lineSeparator());
+
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the file writing exception as needed
+        }
+    }
+
+    /**
+     * Converts customer information to a string representation.
+     *
+     * @param customer The customer object containing the user's personal information.
+     * @return The string representation of the customer information.
+     */
+    private static String customerInformationToString(Customer customer) {
+        return "Identification Number: " + customer.getIdentificationNumber() + ", " +
+                "Date of Birth: " + customer.getDateOfBirth() + ", " +
+                "Address: " + customer.getAddress() + ", " +
+                "Phone Number: " + customer.getPhoneNumber();
+    }
+
+    /**
+     * Retrieves the current date and time in the format "yyyy-MM-dd HH:mm:ss".
+     *
+     * @return The current date and time.
+     */
+    private static String getCurrentDateTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        return formatter.format(date);
     }
 }
